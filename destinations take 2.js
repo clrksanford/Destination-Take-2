e3cb789553905a19e302712de5f4db9ae5f0b733
clrksanford <clarksanford@Clark-Sanfords-MacBook-Pro.local>
2016-07-26 19:33:45 -0500

    Destination Tabs
    This is my first ever website. I wish I had uploaded it earlier to
    track changes as I was actually creating and modifying the files and
    learning how to make all the CSS and Javascript works, but oh well!

---
+++ destinations take 2.js
@@ -0,0 +1,65 @@
+       function hideImage(x) {
+            document.getElementById(x).style.visibility = "hidden";   
+        }
+            
+        function showImage(x) {
+            document.getElementById(x).style.visibility= "visible";   
+        }
+        
+        function showThisImage(x) {
+            switch(x) {
+                
+                case 'Lyon':
+                    showImage('Lyon');
+                    hideImage('Atlanta');
+                    hideImage('Marrakech');
+                    hideImage('Istanbul');
+                    hideImage('Lisbon');
+                    hideImage('Bern');
+                break;
+                
+                case 'Atlanta':
+                    hideImage('Lyon');
+                    showImage('Atlanta');
+                    hideImage('Marrakech');
+                    hideImage('Istanbul');
+                    hideImage('Lisbon');
+                    hideImage('Bern');
+                break;
+                
+                case 'Marrakech':
+                    hideImage('Lyon');
+                    hideImage('Atlanta');
+                    showImage('Marrakech');
+                    hideImage('Istanbul');
+                    hideImage('Lisbon');
+                    hideImage('Bern');
+                break;
+                    
+                case 'Istanbul':
+                    hideImage('Lyon');
+                    hideImage('Atlanta');
+                    hideImage('Marrakech');
+                    showImage('Istanbul');
+                    hideImage('Lisbon');
+                    hideImage('Bern');
+                break;
+                
+                case 'Lisbon':
+                    hideImage('Lyon');
+                    hideImage('Atlanta');
+                    hideImage('Marrakech');
+                    hideImage('Istanbul');
+                    showImage('Lisbon');
+                    hideImage('Bern');
+                break;
+                
+                case 'Bern':
+                    hideImage('Lyon');
+                    hideImage('Atlanta');
+                    hideImage('Marrakech');
+                    hideImage('Istanbul');
+                    hideImage('Lisbon');
+                    showImage('Bern');
+            }
+        }
